a08aefe7099d7b1be886c58f8030fd20f716c44a
Krzysztof Barczynski <krzysztof.barczynski@roclets.net>
2014-04-16 20:47:34 -0700

    Getting boxen on track

--- manifests/site.pp
+++ manifests/site.pp
@@ -53,6 +53,8 @@ Homebrew::Formula <| |> -> Package <| |>
 
 node default {
   # core modules, needed for most things
+  include python
+  include python::virtualenvwrapper
   include dnsmasq
   include git
   include hub
@@ -60,15 +62,91 @@ node default {
   include vagrant
   include alfred
   include vlc
-  include osx
+  include osx::dock::autohide
+  include osx::finder::show_all_on_desktop
+  include osx::universal_access::ctrl_mod_zoom
+  include osx::dock::clear_dock
+  #include osx
+  include vim
+  vim::bundle { [
+    'scrooloose/syntastic',
+    'sjl/gundo.vim',
+    'scrooloose/nerdtree'
+  ]: }
+
+  vim::bundle { 'Bogdanp/rbrepl.vim.git': }
+  vim::bundle { 'Lokaltog/vim-powerline.git': }
+  vim::bundle { 'Raimondi/delimitMate.git': }
+  vim::bundle { 'Valloric/YouCompleteMe.git': }
+  vim::bundle { 'digitaltoad/vim-jade.git': }
+  vim::bundle { 'epeli/slimux.git': }
+  vim::bundle { 'godlygeek/tabular': }
+  vim::bundle { 'guns/vim-clojure-static.git': }
+  vim::bundle { 'hallison/vim-markdown': }
+  vim::bundle { 'heavenshell/vim-jsdoc.git': }
+  vim::bundle { 'kchmck/vim-coffee-script.git': }
+  vim::bundle { 'kien/ctrlp.vim': }
+  vim::bundle { 'kien/rainbow_parentheses.vim.git': }
+  vim::bundle { 'mattn/zencoding-vim.git': }
+  vim::bundle { 'mileszs/ack.vim.git': }
+  vim::bundle { 'ndhoule/vim-ragtag.git': }
+  vim::bundle { 'rodjek/vim-puppet.git': }
+  vim::bundle { 'slim-template/vim-slim.git': }
+  vim::bundle { 'terryma/vim-multiple-cursors.git': }
+  vim::bundle { 'tomtom/tcomment_vim.git': }
+  vim::bundle { 'tpope/vim-fugitive': }
+  vim::bundle { 'tpope/vim-haml.git': }
+  vim::bundle { 'tpope/vim-repeat.git': }
+  vim::bundle { 'tpope/vim-surround': }
+  vim::bundle { 'vim-ruby/vim-ruby': }
+  vim::bundle { 'vim-scripts/Rename.git': }
+  vim::bundle { 'vim-scripts/YankRing.vim.git': }
+  vim::bundle { 'vim-scripts/bufexplorer.zip.git': }
+  vim::bundle { 'vim-scripts/bufkill.vim.git': }
+  vim::bundle { 'vim-scripts/fakeclip.git': }
+  vim::bundle { 'vim-scripts/guicolorscheme.vim': }
+  vim::bundle { 'vim-scripts/jQuery.git': }
+  vim::bundle { 'vim-scripts/scratch.vim.git': }
+
+  ## Snipmate Plugins
+  vim::bundle { 'garbas/vim-snipmate.git': }
+  vim::bundle { 'MarcWeber/vim-addon-mw-utils.git': }
+  vim::bundle { 'tomtom/tlib_vim.git': }
+  vim::bundle { 'honza/vim-snippets.git': }
+
+  ## Themes
+  vim::bundle { 'altercation/vim-colors-solarized.git': }
+  vim::bundle { 'tomasr/molokai.git': }
+  vim::bundle { 'trapd00r/neverland-vim-theme.git': }
+
+
+  repository { "/Users/${::boxen_user}/.dotfiles":
+    source  => "${::github_login}/dotfiles-1"
+  }
+	file { "/Users/${::boxen_user}/.vim/swap":
+    ensure => directory
+	}
+	file { "/Users/${::boxen_user}/.vim/backup":
+    ensure => directory
+	}
+
+
+  file { "${vim::vimrc}":
+    target  => "/Users/${::boxen_user}/.dotfiles/.vimrc",
+    require => Repository["/Users/${::boxen_user}/.dotfiles"]
+  }
+  osx::recovery_message { 'If this Mac is found, please itsaloon@gmx.com': }
   include libreoffice
   include virtualbox
   include packer
   include dropbox
   include macvim
-  include vim
   include induction
   include tmux
+  include chrome
+  include eclipse::jee 
+  include evernote
+#  include chicken_of_the_vnc
 
   # fail if FDE is not enabled
   if $::root_encrypted == 'no' {
@@ -82,6 +160,7 @@ node default {
 
   # default ruby versions
   ruby::version { '1.9.3': }
+  ruby::version { '1.9.3-p484': }
   ruby::version { '2.0.0': }
   ruby::version { '2.1.0': }
   ruby::version { '2.1.1': }
@@ -91,7 +170,8 @@ node default {
     [
       'ack',
       'findutils',
-      'gnu-tar'
+      'gnu-tar',
+      'csshx'
     ]:
   }
 
--- modules/people/manifests/gad0lin.pp
+++ modules/people/manifests/gad0lin.pp
@@ -4,17 +4,17 @@ class people::gad0lin {
 
   # Changes the default shell to the zsh version we get from Homebrew
   # Uses the osx_chsh type out of boxen/puppet-osx
-  osx_chsh { $::luser:
-    shell   => '/opt/boxen/homebrew/bin/zsh',
-    require => Package['zsh'],
-  }
+#  osx_chsh { $::luser:
+#    shell   => '/opt/boxen/homebrew/bin/zsh',
+#    require => Package['zsh']
+#  }
   include macvim
 
-  file_line { 'add zsh to /etc/shells':
-    path    => '/etc/shells',
-    line    => "${boxen::config::homebrewdir}/bin/zsh",
-    require => Package['zsh'],
-  }
+#  file_line { 'add zsh to /etc/shells':
+#    path    => '/etc/shells',
+#    line    => "${boxen::config::homebrewdir}/bin/zsh",
+#    require => Package['zsh'],
+#  }
 
   ##################################
   ## Facter, Puppet, and Envpuppet##
